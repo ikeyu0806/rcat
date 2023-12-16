37e29ed42a854114007370b8037b49fcfaee9c64
ikeyu0806 <ikeyu0806@gmail.com>
2023-12-16 09:46:13 +0900

    display filepath using clap library

--- REAMDME.md
+++ REAMDME.md
@@ -1,8 +1,13 @@
 rust試したくて作ったcatみたいなCLIツール作成プロジェクト
 
-### プロジェクト立ち上げ
+## プロジェクト立ち上げ
 ```
 docker-compose build
 docker-compose run rcat cargo init
 docker-compose run rcat cargo add clap
+```
+
+## 実行
+```
+docker-compose run rcat cargo run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use clap::{Command, Arg};
+
 fn main() {
-    println!("Hello, world!");
+    let filepath_arg: Arg = Arg::new("filepath")
+        .required(true);
+
+    let command: Command = Command::new("rcat")
+        .author("Yuki Ikegaya")
+        .version("v1.0.0")
+        .arg(filepath_arg);
+    
+    match command.try_get_matches() {
+        Ok(m) => {
+            let filepath = m.get_one::<String>("filepath").unwrap();
+
+            println!("{}", filepath);
+        },
+        Err(e) => {
+            println!("{}", e);
+        }
+    }
 }
